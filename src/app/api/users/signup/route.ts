c47d24c665c1707b6f4e05903fbf3d494ec0b835
anushkadeshpande <anushkaa5000@gmail.com>
2024-03-25 15:26:48 +0530

    feat: Added signup backend logic

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,10 +1,13 @@
 import { connect } from "@/dbConfig/dbConfig";
 import User from '@/models/userModel'
 import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from 'bcryptjs'
 
-
-connect()
-
+try {
+  connect()
+} catch(ex) {
+  console.error("Database connection could not be established! ", ex)
+}
 export async function POST(request: NextRequest) {
   try {
     const reqBody = request.json()
@@ -17,10 +20,23 @@ export async function POST(request: NextRequest) {
     const user = await User.findOne({email})
     
     if(user) {
-      NextResponse.json({error: "User already exists!"}, {status: 400})
-    } else {
-      
-    }
+      return NextResponse.json({error: "User already exists!"}, {status: 400})
+    } 
+
+    const salt = bcryptjs.genSaltSync(10);
+    const hashedPassword = await bcryptjs.hash(password, salt)
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    })
+
+    const savedUser = await newUser.save()
+
+    console.log(savedUser)
+
+    // TODO: Send verification email
   } catch(ex: any) {
     return NextResponse.json({ex: ex.message}, {status: 500})
   }
